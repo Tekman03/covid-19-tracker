f746cf3c2ba28d43037b3f774b40f571b69129c9
Craig Ballingall <craig@tekman03.co.uk>
2020-10-04 20:24:44 +0100

    Add graph command to help command

--- cogs/Help.py
+++ cogs/Help.py
@@ -48,11 +48,11 @@ class Help(commands.Cog):
                     value=f"Views graphical statistics. If no args provided return linear graph for total cases. You can find countries with **full name** or **[ISO-3166-1](https://fr.wikipedia.org/wiki/ISO_3166-1)**.\n __Examples__ : `{ctx.prefix}stats us`, `{ctx.prefix}s log usa`, `{ctx.prefix}stats log`, `{ctx.prefix}s`",
                     inline=False
                 )
-                # embed.add_field(
-                #     name=f"📈 **`{ctx.prefix}<g | graph> <proportion> <deaths | confirmed | recovered | active> <top | country[]>`**",
-                #     value=f"Views graphical statistics. You can find countries with **full name** or **[ISO-3166-1](https://fr.wikipedia.org/wiki/ISO_3166-1)**.\n\n **Proportion**: This is the value / population * 100 \n\n __Examples__ : `{ctx.prefix}graph proportion top`, `{ctx.prefix}g proportion deaths us gb it es mx fr`, `{ctx.prefix}g proportion active gb`",
-                #     inline=False
-                # )
+                embed.add_field(
+                    name=f"📈 **`{ctx.prefix}<g | graph> <proportion | daily | total> <deaths | confirmed | recovered> <top | country[] | world>`**",
+                    value=f"Views graphical statistics. You can find countries with **full name** or **[ISO-3166-1](https://fr.wikipedia.org/wiki/ISO_3166-1)**.\n\n **Proportion**: This is the value / population * 100 \n\n __Examples__ : `{ctx.prefix}graph proportion top`, `{ctx.prefix}g proportion deaths us gb it es mx fr`, `{ctx.prefix}g proportion active gb`",
+                    inline=False
+                )
                 embed.add_field(
                     name=f"📈 **`{ctx.prefix}country <country>`**",
                     value=f"Views information about multiple chosen country. You can either use **autocompletion** or **[ISO-3166-1](https://fr.wikipedia.org/wiki/ISO_3166-1)**.\n __Examples__ : `{ctx.prefix}country fr usa it gb`",
